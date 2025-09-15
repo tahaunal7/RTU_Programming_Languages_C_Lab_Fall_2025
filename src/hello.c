8d0049a69d814806b4508168bc0d41ab77c29e82
tahaunal7 <Mehmet-Taha.Unal@edu.rtu.lv>
2025-09-15 14:56:56 +0000

    src/hello.c document changed

--- src/hello.c
+++ src/hello.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]) {
   // This is your first C program my friend
   printf("Hello, RTU World from C Lab in 2025!\n");
   printf("You passed %d argument(s).\n", argc - 1);
+  printf("Hello from TAHA! \n");
   for (int i = 1; i < argc; ++i) {
     printf("  arg[%d] = %s\n", i, argv[i]);
   }
